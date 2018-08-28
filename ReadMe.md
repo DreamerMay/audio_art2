f51c50cb93ec60ff765118f3e5986c60b144c60a
Sherine <sherinemay79@gmail.com>
2018-08-28 15:39:25 +1000

    added particles

---
+++ ReadMe.md
@@ -0,0 +1,22 @@
+##Detect BMP
+
+
+filtering the audio, in this example with a low-pass filter
+calculating peaks using a threshold
+grouping interval counts and then tempo counts
+For the threshold I used an arbitrary value of .98 of the range between maximum and minimum values; when grouping I added some additional checks and arbitrary rounding to avoid possible infinite loops and make it an easy-to-debug sample.
+
+Note that commenting is scarce to keep the sample implementation brief because:
+
+the logic behind processing is explained in the referenced article
+the syntax can be referenced in the API docs of the related methods
+
+
+## Adding soundcloud :
+https://codepen.io/DonKarlssonSan/post/fun-with-web-audio-api
+
+# run local drive to test avoice music error use in terminal
+ python -m SimpleHTTPServer
+
+ P5.js example:
+ https://p5js.org/examples/input-storing-input.html
--- js/main.js
+++ js/main.js
@@ -4,22 +4,33 @@ console.log("Audio_art2");
 // Global varables
 //=========================
 
-let song;
-let button;
-let canvas;
-let mic;
-let amplitude;
+let button, canvas;
+let song, mic, amplitude, vol, fft;
 
-//=========================
-// Setup Microphone
-//=========================
-// Activate mic
+// Beat rectangle parameters
+let rectRotate = true;
+let rectMin = 15;
+let rectOffset = 20;
+let numRects = 10;
+
+// Beat Detect
+let beatHoldFrames = 30;
+let beatThreshold = 0.11;
+let beatCutoff = 0;
+let beatDecayRate = 0.98;
+let framesSinceLastBeat = 0;
+
+//FFT settings
+let smoothing = 0.8;
+let binCount = 1024;
+let particles = new Array(binCount);
+
+
 
 function startMic(){
-  mic = new p5.AudioIn();
-  mic.start();
-  console.log('mic activate');
-
+    mic = new p5.AudioIn();
+    mic.start();
+    console.log('mic activate');
 }
 
 function toggleSong() {
@@ -35,16 +46,33 @@ function toggleSong() {
 }
 
 function preload() {
-  song = loadSound('assets/Intro.mp3');
+  song = loadSound('assets/the_depths.mp3');
 }
+
+//=========================
+// AUDIO & CANVAS SETUP
+//=========================
+
 
 function setup() {
   canvas = createCanvas(windowWidth, windowHeight);
   background(0);
+  noStroke();
+
   canvas.parent('audio_canvas');
   width = windowWidth;
   height = windowHeight;
   width < height ? smlAxis = width : smlAxis = height;
+
+
+
+  // AUDIO DATA SETUP
+  amplitude = new p5.Amplitude();
+  amplitude.setInput(song);
+  amplitude.smooth(0.9);
+
+  fft = new p5.FFT(smoothing, binCount);
+  fft.setInput(song);
 
   micButton = createButton('Mic');
   micButton.mouseClicked(startMic);
@@ -52,70 +80,175 @@ function setup() {
   playButton = createButton('Play');
   playButton.mouseClicked(toggleSong);
 
+  // instantiate the particles
+  for (var i = 0; i < particles.length; i++) {
+    let x = map(i, 0, binCount, 0, width * 2);
+    let y = random(0, height);
+    let position = createVector(x, y);
+    particles[i] = new Particle(position);
+  }
+
 }
 
 function draw() {
   width = windowWidth;
   height = windowHeight;
+  background(0);
 
   if (mic === undefined ) {
     return;
   } else {
     // circle for mic input
     let vol = mic.getLevel();
-    // console.log(vol);
     fill(255, 204, 0);
     stroke('red');
 
     // Draw an ellipse with height based on volume
-    let h = map(vol*100, 0, 1, height, 0);
-    ellipse(width/2, h - 25, 50, 50);
+    let eh = map(vol*100, 0, 1, height, 0);
+    ellipse(width/2, eh - 25, 50, 50);
 
 
     // art for amplitude (pink)
-    amplitude = new p5.Amplitude();
-    let level = amplitude.getLevel();
-    fill('#fae')
-    // console.log(level);
-    let a = map(amplitude, 0, 10, height, 0);
-    ellipse(width/3, h - 25, 50, 50);
+    let rms = amplitude.getLevel();
+
+    fill('#fae');
+    stroke(255);
+    ellipse(width/3, height/2, 50+rms*300, 50+rms*300);
+
 
     // Threshold art (green)
-    let threshold = 0.1;
-    if (vol > threshold) {
+    let volThreshold = 0.1;
+    if (rms > volThreshold) {
       stroke(255);
-      fill(155,100);
-      rect(random(40, width), random(height), vol*500, vol*500);
+      fill('#42fc96');
+      rect(random(40, width), random(height), vol*10000, vol*10000);
     }
 
-    let y = map(vol, 0, 1, height, 0);
-    let ythreshold = map(threshold, 0, 1, height, 0);
+    let yt = map(rms, 0, 1, height, 0);
+    let ythreshold = map(volThreshold, 0, 1, height, 0);
 
     noStroke();
-    fill('green');
+    fill(255);
     rect(0,0, 20, height);
 
     fill(255);
-    rect(0, y, 20, y);
+    rect(0, yt, 20, yt);
     stroke(255);
     line(0, ythreshold, 19, ythreshold);
+
+    // FFT spectrum art
+    let spectrum = fft.analyze(binCount);
+
+    beginShape();
+
+    fill('rgba(255,93,115,1)');
+    stroke('rgba(50,50,50,1)');
+    strokeWeight(1);
+    for (let i = 0; i < spectrum.length; i++) {
+      vertex(i, map(spectrum[i], 0, 255, height, 0) );
+    }
+    endShape();
+
+    //========================
+    // Beat detector art
+    detectBeat(rms);
+    rectMode(CENTER);
+
+    // distort the rectable based on amp
+    let distortDiam = map(rms, 0, 1, 0, 1200);
+    let w = rectMin;
+    let h = rectMin;
+
+    // distortion direction shift each beat
+    if (rectRotate) {
+      var rotation = PI / 2;
+    } else {
+      var rotation = PI /3;
+    }
+
+    let rectCenter = createVector(width/3, height/2);
+    push();
+
+      // draw the rectangles
+      for (var i = 0; i < numRects; i++) {
+        let x = rectCenter.x + rectOffset * i;
+        let y = rectCenter.y + distortDiam/2;
+        // rotate around the center of this rectangle
+        translate(x,y);
+        rotate(rotation);
+        rect(0, 0, rectMin, rectMin + distortDiam);
+      }
+
+    pop();
+
+     // Particles
+     for (var i = 0; i < binCount; i++) {
+       let thisLevel = map(spectrum[i], 0, 255, 0, 1);
+
+       particles[i].update(thisLevel);
+       particles[i].draw();
+       particles[i].position.x = map(i, 0, binCount, 0, width*2);
+     }
   }
 }
 
 
+//=========================
+// BPM detector
+//=========================
+function detectBeat(level) {
+  if (level > beatCutoff && level > beatThreshold) {
+    onBeat();
+    beatCutoff = level * 1.2;
+    framesSinceLastBeat = 0;
+  } else {
+    if (framesSinceLastBeat <= beatHoldFrames) {
+      framesSinceLastBeat ++ ;
+    } else {
+      beatCutoff *= beatDecayRate;
+      beatCutoff = Math.max(beatCutoff, beatThreshold);
+    }
+  }
+}
+
+function onBeat() {
+  // backgrondColor = color( random(200,255), random(200,255), random(200,255) );
+  rectRotate = !rectRotate;
+}
 
 
 //=========================
-// Get BPM
+// Particle SETUP
 //=========================
-// draw circle with the data of the output
+// Particle class
 
-  // console.log(vol);
+let Particle = function(position) {
+  this.position = position;
+  this.scale = random(0, 1);
+  this.speed = createVector(0, random(0,10));
+  this.color = [255, 255, 255];
+}
+
+let theyExpand = 1;
+
+// use FFT bin level to change speed and diameter
+Particle.prototype.update = function(someLevel) {
+  this.position.y += this.speed.y / (someLevel*2);
+  if (this.position.y > height) {
+    this.position.y = 0;
+  }
+  this.diameter = map(someLevel, 0, 1, 0, 100) * this.scale * theyExpand;
+}
+
+Particle.prototype.draw = function() {
+  fill(this.color);
+  ellipse(
+    this.position.x, this.position.y, this.diameter, this.diameter
+  );
+};
 
 
-// set gain to control avtive and not active
-
-//=========================
-// Setup global varables for data
-//=========================
-// get the source output data (understand the data)
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+  background(0);
+}
