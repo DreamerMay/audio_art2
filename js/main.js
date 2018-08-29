68ead79fdfe534a3ddb94d38329dcfb3c591cf96
Sherine <sherinemay79@gmail.com>
2018-08-29 19:42:12 +1000

    attempting my beat creating

--- js/main.js
+++ js/main.js
@@ -5,13 +5,9 @@ console.log("Audio_art2");
 //=========================
 
 let button, canvas;
-let song, mic, currentSource, amplitude, rms, spectrum, vol, fft;
+let song, mic, currentSource, amplitude, rms, spectrum, volMic, fft;
 
-// Beat rectangle parameters
-let rectRotate = true;
-let rectMin = 15;
-let rectOffset = 20;
-let numRects = 10;
+
 
 // Beat Detect
 let beatHoldFrames = 30;
@@ -37,6 +33,8 @@ function preload() {
   song = loadSound('assets/the_depths.mp3');
 }
 
+
+
 //=========================
 // AUDIO & CANVAS SETUP
 //=========================
@@ -61,7 +59,7 @@ function setup() {
   amplitude.smooth(0.9);
 
   fft = new p5.FFT(smoothing, binCount);
-  fft.setInput(mic);
+  fft.setInput(song);
 
   //===============
   // USER CONTROL
@@ -114,165 +112,254 @@ function draw() {
 
 
       // yellow circle for mic input
-      let vol = mic.getLevel();
+      let volMic = mic.getLevel();
       rms = amplitude.getLevel();
       spectrum = fft.analyze(binCount);
 
       if (song.isPlaying()) {
         fill(0);
         stroke(0)
+        console.log(rms);
       } else {
         fill(255, 204, 0);
         stroke('red');
 
         // Draw an ellipse with height based on volume
-        let eh = map(vol*20, 0, 1, height, 0);
+        let eh = map(volMic*20, 0, 1, height, 0);
         ellipse(width*(2/3), eh - 100, 50, 50);
       }
-
-
 
 
       // Threshold art (green)
       let volThreshold = 0.1;
       // console.log(rms);
-      if (vol > volThreshold) {
-        stroke('#fcf688');
-        fill('#fcf688');
-        rect(random(40, width), random(height), vol*500, vol*500);
+      if (volMic > volThreshold) {
+        stroke('#44a8ff');
+        fill('#44a8ff');
+        rect(random(40, width), random(height), volMic*500, volMic*500);
       }
 
-      let yt = map(vol, 0, 1, height, 0);
-      let ythreshold = map(volThreshold, 0, 1, height, 0);
+      //==================
+      // Threshold long line on the side
+      //=============
+      // let yt = map(vol, 0, 1, height, 0);
+      // let ythreshold = map(volThreshold, 0, 1, height, 0);
+      //
+      // noStroke();
+      // fill(255);
+      // rect(0,0, 20, height);
+      //
+      // fill(255);
+      // rect(0, yt, 20, yt);
+      // stroke(255);
+      // line(0, ythreshold, 19, ythreshold);
 
-      noStroke();
-      fill(255);
-      rect(0,0, 20, height);
 
-      fill(255);
-      rect(0, yt, 20, yt);
-      stroke(255);
-      line(0, ythreshold, 19, ythreshold);
 
     if (song.isPlaying()) {
-    // art for amplitude (pink)
-    if (rms > 0.01) {
-      fill('#fae');
-      stroke(255);
-      ellipse(width/3, height/2, 50+rms*300, 50+rms*300);
-    }
+      // =========================
+      // Rectangle => Amplitude
+      //===========================
+      noFill();
+      stroke('#fcf688');
 
-    // FFT spectrum art
+      if (rms >= 0.15) {
+        strokeWeight(2);
+        rect((windowWidth/2)-200, (windowHeight/2) - 50, 400, 200);
+      } if (rms >= 0.2) {
+        strokeWeight(4);
+        rect((windowWidth/2)-250, (windowHeight/2) - 100, 500, 300);
+      } if (rms >= 0.4) {
+        strokeWeight(8);
+        rect((windowWidth/2)-300, (windowHeight/2) - 150, 600, 400);
+      } if (rms >= 0.5) {
+        strokeWeight(12);
+        rect((windowWidth/2)-350, (windowHeight/2) - 200, 700, 500);
+      } if (rms >= 0.6) {
+        strokeWeight(12);
+        rect((windowWidth/2)-400, (windowHeight/2) - 250, 800, 600);
+      }
 
-    beginShape();
 
-    fill('rgba(255,93,115,1)');
-    stroke('rgba(50,50,50,1)');
-    strokeWeight(1);
-    for (let i = 0; i < spectrum.length; i++) {
-      vertex(i, map(spectrum[i], 0, 255, height, 0) );
-    }
-    endShape();
+       //================
+       // Peak detector - 4 side pink angle
+       //================
+       fft.analyze();
+       peakDetect.update(fft);
 
-    //========================
-    // Beat detector art
+       if (peakDetect.isDetected) {
+         fill('#FF0651');
+         noStroke()
+         quad(1200, 260, 1160, 220, 1650, -100, 1650, 100);
+         quad(370, 260, 400, 220, -100, -100, -100, 100);
+         quad(400, 740, 370, 700, -100, 820, -100, 1000);
+         quad(1160, 740, 1200, 700, 1650, 820, 1650, 1000);
+       }
+
+
+    // // art for amplitude (pink)
+    // if (rms > 0.01) {
+    //   fill('#fae');
+    //   stroke(255);
+    //   ellipse(width/3, height/2, 50+rms*300, 50+rms*300);
+    // }
+    //
+    // // FFT spectrum art
+    //
+    // beginShape();
+    //
+    // fill('rgba(255,93,115,1)');
+    // stroke('rgba(50,50,50,1)');
+    // strokeWeight(1);
+    // for (let i = 0; i < spectrum.length; i++) {
+    //   vertex(i, map(spectrum[i], 0, 255, height, 0) );
+    // }
+    // endShape();
+    //
+
+
+
+    // // ========================
+    // // Beat detector art // remove for now.
+    // // ========================
+    //
+    // // Beat rectangle parameters
+    // let rectRotate = true;
+    // let rectMin = 15;
+    // let rectOffset = 10;
+    // let numRects = 5;
+    //
+    // detectBeat(rms);
+    // // rectMode(CENTER);
+    // fill('#42fc96');
+    // noStroke();
+    //
+    // if (rms > 0.01) {
+    //   // distort the rectable based on amp
+    //   let distortDiam = map(rms, 0, 1, 0, 1200);
+    //   let w = rectMin;
+    //   let h = rectMin;
+    //
+    //   //distortion direction shift each beat
+    //   // if (rectRotate) {
+    //   //   var rotation = PI / 2;
+    //   // // } else {
+    //   // //   var rotation = PI / 2;
+    //   // }
+    //
+    //   let rectCenter = createVector(width/3, height/2);
+    //   push();
+    //     // draw the rectangles
+    //     for (var i = 0; i < numRects; i++) {
+    //       let x = rectCenter.y - distortDiam/2 ; // - move left; + move right
+    //       let y = rectCenter.x + rectOffset * i ;
+    //       // rotate around the center of this rectangle
+    //       translate(x, y);
+    //       // rotate(rotation);
+    //       rect(0, 0, rectMin, rectMin + distortDiam);
+    //     }
+    //   pop();
+    //
+    // }
+
+    //======================
+    // My Beat Detector Art
+    //======================
+
+    // Beat rectangle parameters
+    // let rectRotate = true;
+    let rectMin = 15;
+    let rectOffset = 10;
+    // let numRects = 5;
+
     detectBeat(rms);
-    rectMode(CENTER);
+
+    push();
+
     if (rms > 0.01) {
       // distort the rectable based on amp
       let distortDiam = map(rms, 0, 1, 0, 1200);
       let w = rectMin;
       let h = rectMin;
 
-      // distortion direction shift each beat
-      if (rectRotate) {
-        var rotation = PI / 2;
+      rectMode(CENTER);
+
+      fill('#42fc96');
+      noStroke();
+
+
+
+
+      //left beat rectangle
+      rect(350, 400, 20, 200);
+      rect(300, 350, 20, 300);
+      rect(250, 300, 20, 400);
+
+      //right beat rectangle
+      rect(1200, 400, 20, 200);
+      rect(1250, 350, 20, 300);
+      rect(1300, 300, 20, 400);
+
+    pop();
+
+
+    //=========================
+    // BPM detector
+    //=========================
+    function detectBeat(level) {
+      if (level > beatCutoff && level > beatThreshold) {
+        onBeat();
+        beatCutoff = level * 1.2;
+        framesSinceLastBeat = 0;
       } else {
-        var rotation = PI /3;
-      }
-
-      let rectCenter = createVector(width/3, height/2);
-      push();
-
-        // draw the rectangles
-        for (var i = 0; i < numRects; i++) {
-          let x = rectCenter.x + rectOffset * i;
-          let y = rectCenter.y + distortDiam/2;
-          // rotate around the center of this rectangle
-          translate(x,y);
-          rotate(rotation);
-          rect(0, 0, rectMin, rectMin + distortDiam);
+        if (framesSinceLastBeat <= beatHoldFrames) {
+          framesSinceLastBeat ++ ;
+        } else {
+          beatCutoff *= beatDecayRate;
+          beatCutoff = Math.max(beatCutoff, beatThreshold);
         }
-
-      pop();
+      }
     }
-    //==================
-     // Particles
-     for (var i = 0; i < binCount; i++) {
-       let thisLevel = map(spectrum[i], 0, 255, 0, 1);
 
-       particles[i].update(thisLevel);
-       particles[i].draw();
-       particles[i].position.x = map(i, 0, binCount, 0, width*2);
-     }
+    function onBeat() {
+      // backgrondColor = color( random(200,255), random(200,255), random(200,255) );
+      rectRotate = !rectRotate;
+    }
 
-     //================
-     // Peak detector
-     fft.analyze();
-     peakDetect.update(fft);
-     fill('blue');
+    // //==================
+    //  // Particles
+    //  for (var i = 0; i < binCount; i++) {
+    //    let thisLevel = map(spectrum[i], 0, 255, 0, 1);
+    //
+    //    particles[i].update(thisLevel);
+    //    particles[i].draw();
+    //    particles[i].position.x = map(i, 0, binCount, 0, width*2);
+    //  }
+    //
 
-     if (peakDetect.isDetected) {
-       ellipseWidth = 200;
-     } else {
-       ellipseWidth *= 0.95;
-     }
-
-     ellipse(width*(2/3), height*(1/3), ellipseWidth, ellipseWidth)
-
-     //====================
-     // // Noise Art
-     // beginShape();
-     // fill('#42fc96');
-     // let xoff = 0;
-     //
-     // for (let xn = 0; xn < width; xn += 10) {
-     //   let yn = map(noise(xoff, yoff), 0, 1, 300, 1200);
-     //
-     //   vertex(xn, yn);
-     //   xoff += 0.05;
-     // }
-     // yoff += 0.01;
-     // vertex(width, height);
-     // vertex(0, height);
-     // endShape(CLOSE);
-     }
-  }
-}
-
-
-//=========================
-// BPM detector
-//=========================
-function detectBeat(level) {
-  if (level > beatCutoff && level > beatThreshold) {
-    onBeat();
-    beatCutoff = level * 1.2;
-    framesSinceLastBeat = 0;
-  } else {
-    if (framesSinceLastBeat <= beatHoldFrames) {
-      framesSinceLastBeat ++ ;
-    } else {
-      beatCutoff *= beatDecayRate;
-      beatCutoff = Math.max(beatCutoff, beatThreshold);
+    //
+    //  //====================
+    //  // // Noise Art
+    //  // beginShape();
+    //  // fill('#42fc96');
+    //  // let xoff = 0;
+    //  //
+    //  // for (let xn = 0; xn < width; xn += 10) {
+    //  //   let yn = map(noise(xoff, yoff), 0, 1, 300, 1200);
+    //  //
+    //  //   vertex(xn, yn);
+    //  //   xoff += 0.05;
+    //  // }
+    //  // yoff += 0.01;
+    //  // vertex(width, height);
+    //  // vertex(0, height);
+    //  // endShape(CLOSE);
     }
   }
 }
 
-function onBeat() {
-  // backgrondColor = color( random(200,255), random(200,255), random(200,255) );
-  rectRotate = !rectRotate;
-}
+
+
 
 
 //=========================
